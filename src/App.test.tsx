6fba3c4f6fab34eb2b187ca0df18451766673bfa
Simon Dell <simon.dell@founders4schools.org.uk>
2020-04-08 22:59:33 +0100

    My first hook, and first react-testing-library component tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Shows a counter', () => {
+  const { getByText } = render(<App />)
+  const counter = getByText(/\d clicks/i)
+
+  expect(counter).toBeInTheDocument()
+})
+
+test('Shows a button', () => {
+  const { getByText } = render(<App />)
+  const button = getByText(/Click me!/i)
+
+  expect(button).toBeInTheDocument()
+})
+
+test('Clicking the button updates the counter', () => {
+  const tree: any = render(<App />)
+  const button = tree.getByText(/Click me!/)
+
+  const clicks: number[] = [1, 2, 3, 4, 5]
+  clicks.forEach((click: number): void => {
+  	fireEvent.click(button)
+
+  	const counter = tree.getByText(new RegExp(`${click} clicks`))
+  	expect(counter).toBeInTheDocument()
+  })
+})
